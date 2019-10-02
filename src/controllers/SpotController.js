0b1e7fe17dbdec7f7e1989b00874a4fc6c074c25
antoniojunior1890 <antonio.junior1890@hotmail.com>
2019-10-02 17:07:19 -0300

    SpotController - store - upload

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,10 +1,27 @@
-// const User = require('../models/User');
+const User = require('../models/User');
+const Spot = require('../models/Spot');
 
 module.exports = {
     async store(req, res){ 
-        console.log(req.body);
-        console.log(req.file);
+
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
         
-        return res.json({ok: true});
+        const user = await User.findById(user_id);
+
+        if(!user){
+            return res.status(400).json({ error: 'Usuario não existe'}); 
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbanail: filename,
+            company,
+            techs : techs.split(',').map(tech => tech.trim()),
+            price,
+        })
+
+        return res.json(spot);
     }
 }
